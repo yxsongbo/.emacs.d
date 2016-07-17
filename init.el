ca053223c40ab6712508de7803dd3a57c1dba595
Song Bo <yxsongbo@gmail.com>
2016-07-17 20:46:56 +0800

    org init file

--- init.el
+++ init.el
@@ -1,92 +1,15 @@
-;; 很早以前，使用下面两行来避免乱码
-;; (set-fontset-font "fontset-default"
-;; 'gb18030 '("Microsoft YaHei" . "unicode-bmp"))
-
-;; 利用子龙山人的melpa镜像
-;;(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
-;;                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")))
-
-
-;;冯书的package配置
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa") t)
-
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+        ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
 (package-initialize)
-(setq package-unsigned-archives '("gnu"))
 
-(setq org-agenda-files (list "~/Dropbox/doc/jw"
-			     "~/Dropbox/doc/diary.org"))
-;; use-package
-(require 'use-package)
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
-
-;;Open recently opened file:recentf-mode
-;; 下面两行是我自己的配置
-(recentf-mode 1)
-(global-set-key (kbd "C-x f") 'recentf-open-files)
-
-
-(require 'ido)
-(ido-mode t)
-
-(require 'helm-config)
-
-(require 'evil)
-(evil-mode 1)
-
-
-(define-key global-map "\C-ca" 'org-agenda)
-;; chinese-pyim 的简单配置
-;; 补充：emacs注释的方法
-;; 先选中区域，然后M-x comment-region，会根据mode自动选择注释类型。
-(require 'chinese-pyim)
-(require 'chinese-pyim-greatdict)
-(chinese-pyim-greatdict-enable)
-(setq default-input-method "chinese-pyim")
-(global-set-key (kbd "C-\\") 'toggle-input-method)
-;;选词框设置,可以让字体不能大了可以大些吗不能再大了
-;; (setq pyim-use-tooltip 'popup)
-;; (setq x-gtk-use-system-tooltips t)
-;; 调涨选词框的大小
-;; 算了，还是让emacs自己调整选词框大小吧(set-face-attribute 'popup-tip-face nil :height 200)
-
-
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-  
-;;(load-theme 'spacemacs-dark t)
-
-
-
-
-;;有道词典
-;; Enable Cache
-;;(setq url-automatic-caching t)
-
-;; Example Key binding
-;;(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
-;;(global-set-key (kbd "C-c s") 'youdao-dictionary-play-voice-at-point)
-
-;; Integrate with popwin-el (https://github.com/m2ym/popwin-el)
-;; (push "*Youdao Dictionary*" popwin:special-display-config)
-
-;; Set file path for saving search history
-;; (setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao")
-
-;; Enable Chinese word segmentation support (支持中文分词)
-;; (setq youdao-dictionary-use-chinese-word-segmentation t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/doc/jw" "~/Dropbox/doc/diary.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
---
+++ myinit.el
@@ -0,0 +1,98 @@
+
+(tool-bar-mode -1)
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "<f5>") 'revert-buffer)
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package org-bullets
+:ensure t
+:config
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package chinese-pyim
+    :ensure nil
+    :config
+    (use-package chinese-pyim-greatdict
+        :ensure nil
+        :config (chinese-pyim-greatdict-enable))
+    (setq default-input-method "chinese-pyim")
+    (global-set-key (kbd "C-\\") 'toggle-input-method))
+
+(use-package auto-complete
+:ensure t
+:init
+(progn
+  (ac-config-default)
+  (global-auto-complete-mode t)
+  ))
+
+;; org-agenda-files 变量的设置
+(setq org-agenda-files (list "~/Dropbox/doc/jw"
+                             "~/Dropbox/doc/diary.org"))
+(define-key global-map "\C-ca" 'org-agenda)
+
+;;Open recently opened file:recentf-mode
+;; 下面两行是我自己的配置
+;; (recentf-mode 1)
+;; (global-set-key (kbd "C-x f") 'recentf-open-files)
+(use-package recentf
+  :ensure nil
+  :bind ("C-x f" . recentf-open-files))
+
+(require 'ido)
+(ido-mode t)
+
+;; helm
+(require 'helm-config)
+;;(helm-mode 1)
+
+(require 'evil)
+(evil-mode 1)
+
+
+;; chinese-pyim 的简单配置
+;; 补充：emacs注释的方法
+;; 先选中区域，然后M-x comment-region，会根据mode自动选择注释类型。
+;; (require 'chinese-pyim)
+;; (require 'chinese-pyim-greatdict)
+;; (chinese-pyim-greatdict-enable)
+;; (setq default-input-method "chinese-pyim")
+;; (global-set-key (kbd "C-\\") 'toggle-input-method)
+;;选词框设置,可以让字体不能大了可以大些吗不能再大了
+;; (setq pyim-use-tooltip 'popup)
+;; (setq x-gtk-use-system-tooltips t)
+;; 调涨选词框的大小
+;; 算了，还是让emacs自己调整选词框大小吧(set-face-attribute 'popup-tip-face nil :height 200)
+
+
+;;(load-theme 'spacemacs-dark t)
+
+;;有道词典
+;; Enable Cache
+;;(setq url-automatic-caching t)
+
+;; Example Key binding
+;;(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+;;(global-set-key (kbd "C-c s") 'youdao-dictionary-play-voice-at-point)
+
+;; Integrate with popwin-el (https://github.com/m2ym/popwin-el)
+;; (push "*Youdao Dictionary*" popwin:special-display-config)
+
+;; Set file path for saving search history
+;; (setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao")
+
+;; Enable Chinese word segmentation support (支持中文分词)
+;; (setq youdao-dictionary-use-chinese-word-segmentation t)
+
+(add-hook 'emacs-startup-hook
+        '(lambda ()
+           (progn
+             (message "My .emacs loaded in %s"
+                      (emacs-init-time)))))
